3799433f1b4ee8fc8bad381eb0496eeeab65c86e
Cyprian Adamski <cyprian.adamski@gmail.com>
2023-03-23 12:09:32 +0100

    fix amount - expextet number (line 42)

--- src/components/CurrencyForm/CurrencyForm.test.js
+++ src/components/CurrencyForm/CurrencyForm.test.js
@@ -4,11 +4,12 @@ import userEvent from '@testing-library/user-event';
 
 describe('Component CurrencyForm', () => {
   const testCases = [
-    { amount: 100, from: 'PLN', to: 'USD' },
-    { amount: 20, from: 'USD', to: 'PLN' },
-    { amount: 200, from: 'PLN', to: 'USD' },
-    { amount: 345, from: 'USD', to: 'PLN' },
+    { amount: '100', from: 'PLN', to: 'USD' },
+    { amount: '20', from: 'USD', to: 'PLN' },
+    { amount: '200', from: 'PLN', to: 'USD' },
+    { amount: '345', from: 'USD', to: 'PLN' },
   ];
+
   for (const testObj of testCases) {
     it('should render without crashing', () => {
       render(<CurrencyForm action={() => {}} />);
@@ -37,7 +38,11 @@ describe('Component CurrencyForm', () => {
 
       // check if action callback was called once and with proper argument
       expect(action).toHaveBeenCalledTimes(1);
-      expect(action).toHaveBeenCalledWith(testObj);
+      expect(action).toHaveBeenCalledWith({
+        amount: parseInt(testObj.amount),
+        from: testObj.from,
+        to: testObj.to,
+      });
     });
     // unmount component
     cleanup();
